4af81a9a321ce1d8356e646e536644cc99d4b069
HeRedBo <hhbjkd@163.com>
2017-06-19 18:22:52 +0800

    index 后端页面

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -42,9 +42,16 @@ Vue.component(
 	require('./components/admin/Table.vue')
 );
 
-Vue.components(
-    
-);
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    linkActiveClass: 'active',
+    routes: routes
+});
 
-
-
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+const app = new Vue(Vue.util.extend({ router },App)).$mount('#app');
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-cn">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>@yield('title') | {{ config('app.name','laravel')}}</title>
+	
+	<!-- Style -->
+	<link rel="stylesheet" type="text/css" href="/css/admin.css" />
+	<style type="text/css"></style>
+	<script type="text/javascript">
+		window.laravel = {!! json_encode([
+			'csrfToken' => csrf_token(),
+			]) !!}
+		window.Name = '{!! config('app.name','laravel') !!}';
+		window.User = {!! auth('admin')->user() !!};
+
+	</script>
+</head>
+<body class="sidebar-mini skin-purple-light" >
+<div id="app"></div>
+<script type="text/javascript" src="/js/admin/admin.js"></script>
+</body>
+</html>
